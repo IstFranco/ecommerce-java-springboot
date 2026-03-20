1badac43bb7ece25b60c5561324d4ad8d75512cd
Franco Oyhenart <franoyhenart07@gmail.com>
2026-03-20 10:56:36 -0300

    feat: update API endpoints to /api/v1 and configure AuthController

--- src/main/java/com/franco/ecommerce/controller/AuthController.java
+++ src/main/java/com/franco/ecommerce/controller/AuthController.java
@@ -1,4 +1,33 @@
 package com.franco.ecommerce.controller;
 
+import com.franco.ecommerce.dto.AuthenticationRequest;
+import com.franco.ecommerce.dto.AuthenticationResponse;
+import com.franco.ecommerce.dto.RegisterRequest;
+import com.franco.ecommerce.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(service.register(request));
+    }
+
 }
